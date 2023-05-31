883260b59799aad0a4a05a83b2aa944a2e03b4fd
thanhdqhe163134 <thanhdqhe163134@fpt.edu.vn>
2023-05-31 10:51:28 +0700

    Testfgdfg

--- src/java/NewServlet.java
+++ src/java/NewServlet.java
@@ -35,7 +35,7 @@ public class NewServlet extends HttpServlet {
             out.println("<title>Servlet NewServlet</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet NewServlfgdfdgdfget at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet NewServlfgdfdgfhfgfghgdfget at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
