38f61800ef6937af6935e49eeb82a4d2bcaa5f50
thanhdqhe163134 <thanhdqhe163134@fpt.edu.vn>
2023-06-06 14:39:09 +0700

    Thiet ke cau truc code cua trang web

---
+++ src/java/dao/VehicleDAO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+import model.Vehicle;
+
+/**
+ *
+ * @author faced
+ */
+public class VehicleDAO {
+
+    public List<Vehicle> getAllVehicles() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/java/model/Vehicle.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author faced
+ */
+public class Vehicle {
+    private String name;
+    private String brand;
+    private String description;
+    private double price;
+
+    public Vehicle(String name, String brand, String description, double price) {
+        this.name = name;
+        this.brand = brand;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
